fe075bcbdc81112c509dce52078f261d748a2559
audxo14 <audxo14@gmail.com>
2016-09-12 23:45:57 +0900

    Modify pcap_test.cpp

--- pcap_test/pcap_test.cpp
+++ pcap_test/pcap_test.cpp
@@ -12,9 +12,21 @@ void pcap_test(const u_char *packet)
 
 	eth_hdr = (struct libnet_ethernet_hdr *)packet;
 	ip_hdr = (struct libnet_ipv4_hdr *)(packet + sizeof(struct libnet_ethernet_hdr));
-	tcp_hdr = (struct libnet
+	//tcp_hdr = (struct libnet
+	
+	printf("Destionation ");
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%02X",eth_hdr->ether_dhost[i]);
+		if(i < 5)
+			printf(".");
+	}
+	//printf("Destination: %x\n",ntohs(eth_hdr->ether_dhost[ETHER_ADDR_LEN]));
 
-	printf("%x\n",ntohs(eth_hdr->ether_type));
+	if (ntohs(eth_hdr->ether_type) == 0x0800)
+		printf("Success %x\n",ntohs(eth_hdr->ether_type));
+	
+
 	printf("%x, %d, %o\n",ntohs(ip_hdr->ip_p),ip_hdr->ip_p,ntohs(ip_hdr->ip_p));
 	//printf("%d!", eth_hdr->ether_type);
 }
