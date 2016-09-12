2915dceae00204259d4518541b06aa3734c367eb
audxo14 <audxo14@gmail.com>
2016-09-12 09:46:25 +0900

    Modify main.c

--- pcap_test/main.c
+++ pcap_test/main.c
@@ -7,19 +7,27 @@
 
 int main()
 {
-	pcap_t *handle;
-	char *dev;
-	char errbuf[PCAP_ERRBUF_SIZE];
-	struct bpf_program fp;
-	char filter_ex[] = "port 23";
-	bpf_u_int32 mask;
-	bpf_u_int32 net;
-	struct pcap_pkthdr header;
-	const u_char *packet;
-
-	pritnf("Sniffing Packets!\n");
+	int index = 1;
+	pcap_t *handle;			//Session handle
+	char *dev;			//The device to sniff on
+	char errbuf[PCAP_ERRBUF_SIZE];	//Error string
+	struct bpf_program fp;		//The compiled filter
+	char filter_ex[] = "port 23";	//The filter expression
+	bpf_u_int32 mask;		//Our netmask
+	bpf_u_int32 net;		//our IP
+	struct pcap_pkthdr header;	//The header that pcap gives us
+	const u_char *packet;		//The actual packet
 	
+	packet = pcap_next_ex(handle, &headeR);
+	while(packet)
+	{
 
-	pcap_test(packet);
+		pritnf("Sniffing Packet No. %d!\n", index);
+		pcap_test(packet);
+	
+		packet = pcap_next_ex(handle, &header);
+		index++;
+	}
+
 	return 0;
 }
