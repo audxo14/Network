a5c91142655243a6f51da13e82cb8abee58a6e06
audxo14 <audxo14@gmail.com>
2016-09-13 19:06:24 +0900

    Modify pcap_test.cpp - Add IP and TCP part

--- pcap_test/pcap_test.cpp
+++ pcap_test/pcap_test.cpp
@@ -8,26 +8,49 @@ void pcap_test(const u_char *packet)
 {
 	const struct libnet_ethernet_hdr *eth_hdr;
 	const struct libnet_ipv4_hdr *ip_hdr;
+	const struct in_addr *ip_addr;
 	const struct libnet_tcp_hdr *tcp_hdr;
 
 	eth_hdr = (struct libnet_ethernet_hdr *)packet;
 	ip_hdr = (struct libnet_ipv4_hdr *)(packet + sizeof(struct libnet_ethernet_hdr));
-	//tcp_hdr = (struct libnet
 	
-	printf("Destionation ");
+	printf("Destionation MAC Address: ");
 	for (int i = 0; i < 6; i++)
 	{
-		printf("%02X",eth_hdr->ether_dhost[i]);
+		printf("%02x",eth_hdr->ether_dhost[i]);
 		if(i < 5)
 			printf(".");
 	}
-	//printf("Destination: %x\n",ntohs(eth_hdr->ether_dhost[ETHER_ADDR_LEN]));
 
+	printf("\nSource MAC Address: ");
+
+        for (int i = 0; i < 6; i++)
+        {
+                printf("%02x",eth_hdr->ether_shost[i]);
+                if(i < 5)
+                        printf(".");
+        }
+	printf("\n");	
 	if (ntohs(eth_hdr->ether_type) == 0x0800)
-		printf("Success %x\n",ntohs(eth_hdr->ether_type));
-	
+	{
+		printf("\nThe next header  is IP header!!!\n");
 
-	printf("%x, %d, %o\n",ntohs(ip_hdr->ip_p),ip_hdr->ip_p,ntohs(ip_hdr->ip_p));
+		unsigned int eth_size = sizeof(struct libnet_ethernet_hdr);
+	        ip_hdr = (struct libnet_ipv4_hdr *)(packet + eth_size);
+		printf("IP source Address: %s\n",inet_ntoa(ip_hdr->ip_src));
+		printf("IP Destination Address: %s\n", inet_ntoa(ip_hdr->ip_dst));
+
+		if (ip_hdr->ip_p == 6)
+		{
+			printf("\nThe protocol is TCP!!\n");
+			unsigned int iph_size = (ip_hdr->ip_hl) * 4;
+			unsigned int tcph_size = (tcp_hdr->th_off) * 4;
+			tcp_hdr = (struct libnet_tcp_hdr *)(packet + eth_size + iph_size);
+			printf("TCP source Port: %x\n", ntohs(tcp_hdr->th_sport));
+			printf("TCP Destination Port: %x\n", ntohs(tcp_hdr->th_dport));
+		}
+	}
+	
 	//printf("%d!", eth_hdr->ether_type);
 }
 
