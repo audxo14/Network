0f06922ac099653a8b1f7882cba49f3d9d8dab3f
audxo14 <audxo14@gmail.com>
2016-10-05 13:46:47 +0900

    Delete files

--- arp_spoof/arp_spoof.h
+++
@@ -1,8 +0,0 @@
-// send_arp.h
-
-#pragma once
-void send_arp(u_char *s_mac, u_char *d_mac, struct in_addr s_ip, struct in_addr d_ip, pcap_t *handle);
-
-void arp_main(u_char *s_mac, u_char *d_mac, struct in_addr s_ip, struct in_addr d_ip, pcap_t *handle);
-
-int get_arp(const u_char *packet, u_char *s_mac, u_char *d_mac, struct in_addr d_ip);
